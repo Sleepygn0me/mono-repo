a4a7bbd753bf9476ab49f738f86b399c26f6867b
Richard Thornton <richt9662@gmail.com>
2025-09-01 16:23:58 +0100

    added cors as it was missing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 const app = express();
+app.use(cors());
 
 app.get("/message", function (request, response) {
   response.json("55 BURGERS 55 FRIES 55 DRINKS");
