8dabe9a232857133fcc9a507470a40501aa36de4
Richard Thornton <richt9662@gmail.com>
2025-09-01 16:20:02 +0100

    removed app.use(cors()); in an attempt to trouble shoot an issue with render.com

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 const app = express();
-app.use(cors());
 
 app.get("/message", function (request, response) {
   response.json("55 BURGERS 55 FRIES 55 DRINKS");
